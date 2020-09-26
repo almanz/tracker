b627672c9028b1bd14dc2c85d17e37d4608b15b8
alihamed <alihamed11@gmail.com>
2020-09-26 12:35:05 -0400

    skeleton code

---
+++ wa/launch.py
@@ -0,0 +1,19 @@
+print("Skeleton code")
+
+# def process_data(data):
+    # print("Beginning data processing...")
+    # modified_data = data + " that has been modified"
+    # sleep(3)
+    # print("Data processing finished.")
+    # return modified_data
+
+def main():
+
+    #
+    # print(data)
+    # modified_data = process_data(data)
+    # print(modified_data)
+    return 0
+
+if __name__ == "__main__":
+    main()
