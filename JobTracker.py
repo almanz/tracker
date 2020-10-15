43855909c82c1174f9277854a6198eed07dcd94c
alihamed <alihamed11@gmail.com>
2020-10-15 11:49:30 -0400

    updates to data insertion, and id creation

--- JobTracker.py
+++ JobTracker.py
@@ -2,35 +2,35 @@
 This class should implement the requirements highlighted in the document linked in the wo package.
 """
 import json
-# import lib for: json-->csv
+import uuid as id
+
 class JobTracker:
 
     global d
     d = {}
-    global id
-    id = 0
-    global rubrik
-    rubrik = ["Company","didApply","Website","Job Title","appLink","Resume","Extras"]
+    global col
+    col = ["Company", "didApply", "Website", "Job Title", "appLink", "Resume", "Extras"]
 
-    # def __init__(self,):
-    #     d = dict()
 
     def addjob(self,lst):
-        dict = {}
-        id = 0
+        print("addjob:")
+        print("lst size: ",len(lst))
         for elem in lst:
-            dict[rubrik[id]] = elem
-            id += 1
-        d["first"] = dict
+            dic = {}
+            # print("size of elem",elem)
+            counter = 0
+            for el in elem:
+                # print("el in elem:",el)
+                dic[col[counter]] = el
+                counter += 1
+            # print("dic is: ",dic)
+            d[str(id.uuid1())] = dic
+            # print("d is: ",d)
 
-    def lookup(self,param):
-        return 0
+    # def lookup(self,param):
+    #     return 0
 
-    def export_to_csv(self):
-        json.dump(d)
-        return 0
-
-    def to_json(self):
-        with open('data.txt', 'w') as outfile:
+    def to_file(self):
+        with open('data.txt', 'w+') as outfile:
             json.dump(d, outfile)
 
--- wa/launch.py
+++ wa/launch.py
@@ -1,20 +1,21 @@
-import JobTracker
-import sys
-
 def main():
     print("in main()")
     data = [("NoseCone Technologies Inc.","yes","https://www.thesmartcone.com/",
             "Experienced Software Engineer","https://ca.indeed.com/viewjob?jk=ae2a14acdc7258ba",
             None,None),("Hatchways","yes","https://ca.indeed.com/viewjob?jk=c71fd8608152d5d3","Jr. Software Developer","none","none")]
+    # data = ("o","o","o","o","o","o","o")
     tracker = JobTracker.JobTracker()
     tracker.addjob(data)
     print("added job.")
-    tracker.to_json()
+    tracker.to_file()
     print("converted to JSON.")
     print("argv arguments:",sys.argv[0])
+    print("names in namespace:",dir())
     print("done!")
     return 0
 
 
 if __name__ == "__main__":
+    import JobTracker
+    import sys
     main()
