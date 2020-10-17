211b6de618a1a72cf2ade7fa5c83d991b5c0eacf
alihamed <alihamed11@gmail.com>
2020-10-17 09:26:50 -0400

    Can be run from script

--- src/launch.py
+++ src/launch.py
@@ -1,23 +1,33 @@
 def main():
     print("in main()")
-    d = sys.argv[1:]
-    print("command lin args: ",d)
-    data = [("NoseCone Technologies Inc.","yes","https://www.thesmartcone.com/",
-             "Experienced Software Engineer","https://ca.indeed.com/viewjob?jk=ae2a14acdc7258ba",
-             None,None),("Hatchways","yes","https://ca.indeed.com/viewjob?jk=c71fd8608152d5d3","Jr. Software Developer","none","none")]
-    # data = ("o","o","o","o","o","o","o")
-    tracker = JobTracker.JobTracker()
-    tracker.addjob(data)
-    print("added job.")
-    tracker.to_file()
-    print("converted to JSON.")
-    print("argv arguments:",sys.argv[0])
-    print("names in namespace:",dir())
-    print("done!")
+    file = sys.argv[1:]
+    print(len(sys.argv))
+    if(len(sys.argv)==1):
+        data = [("NoseCone Technologies Inc.","yes","https://www.thesmartcone.com/",
+        "Experienced Software Engineer","https://ca.indeed.com/viewjob?jk=ae2a14acdc7258ba",
+        None,None),("Hatchways","yes","https://ca.indeed.com/viewjob?jk=c71fd8608152d5d3","Jr. Software Developer","none","none")]
+        # data = ("o","o","o","o","o","o","o")
+        tracker = JobTracker.JobTracker()
+        tracker.addjob(data)
+        print("added job.")
+        tracker.to_file()
+        print("converted to JSON.")
+        print("done!")
+    else:
+        try:
+            if(os.path.isfile(file[1])):
+                print("Do stuff..")
+            else:
+                print("Not a proper file.")
+        except (ValueError,TypeError):
+            print("Sorry!")
+        except:
+            print("Unexpected error.")
+
     return 0
 
 
 if __name__ == "__main__":
     import JobTracker
-    import sys
+    import sys,os
     main()
